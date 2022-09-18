1dfd06572c4173d9ae53e604c751bb94c1d65151
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2022-09-18 07:26:02 +0300

    Create swagger AccessTokenPropertyFilter

---
+++ WebApiCore.Swagger/Filters/AccessTokenPropertyFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+using WebApiCore.AspNetCore.ActionFilters;
+
+namespace WebApiCore.Swagger.Filters
+{
+    public class AccessTokenPropertyFilter<TTokenAttribute> : IOperationFilter
+        where TTokenAttribute : RequireAccessTokenAttribute
+    {
+        public string ParameterName { get; set; }
+
+        public AccessTokenPropertyFilter(string parameterName = "access_token")
+        {
+            ParameterName = parameterName;
+        }
+
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            var hasAuthAttribute = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<TTokenAttribute>()
+                .Any();
+
+            var isGet = context.MethodInfo.GetCustomAttributes(true)
+                .OfType<HttpGetAttribute>()
+                .Any();
+
+            if (hasAuthAttribute)
+            {
+                operation.Parameters.Add(new OpenApiParameter
+                {
+                    Name = ParameterName,
+                    In = isGet
+                        ? ParameterLocation.Query
+                        : ParameterLocation.Header,
+                    Description = "Access token",
+                    Required = true,
+                    Schema = new OpenApiSchema
+                    {
+                        Type = "string"
+                    },
+                    AllowReserved = true
+                });
+            }
+        }
+    }
+}
