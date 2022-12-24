893b031bbd951e1da553bc45381950364da7675c
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2022-12-24 07:18:29 +0200

    Add description

--- WebApiCore.AspNetCore/Mvc/JsonPropertyQueryModelBinderAttribute.cs
+++ WebApiCore.AspNetCore/Mvc/JsonPropertyQueryModelBinderAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace WebApiCore.AspNetCore.Mvc
 {
+    /// <summary>
+    /// Attribute for <see cref="JsonPropertyQueryModelBinder"/> that allows to bind properties from query (when it's doesn't by default)
+    /// </summary>
     public class JsonPropertyQueryModelBinderAttribute : ModelBinderAttribute
     {
         public JsonPropertyQueryModelBinderAttribute() : base(typeof(JsonPropertyQueryModelBinder))
