127496f923fd0bfd473bd4273fa4c7546ab14a27
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2022-12-24 07:16:19 +0200

    Create InternalPropertyAttribute.cs

---
+++ WebApiCore/Attributes/InternalPropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebApiCore.Attributes
+{
+    /// <summary>
+    /// Marks property to indicate that it should be concealed from public vision
+    /// </summary>
+    [AttributeUsage(validOn: AttributeTargets.Property)]
+    public class InternalPropertyAttribute : Attribute
+    {
+        public InternalPropertyAttribute()
+        {
+
+        }
+    }
+}
